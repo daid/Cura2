d158f21d01c3d3c44c3d2101ffb04f5699d12519
daid <daid303@gmail.com>
2014-04-25 14:57:02 +0200

    Remove print.

--- Cura/localization.py
+++ Cura/localization.py
@@ -6,9 +6,7 @@ from Cura.resources import getResourcePath
 def setup():
     languages = ['en']
 
-    #TODO: Resource path
     locale_path = getResourcePath('locale')
-    print locale_path
     locale_path = os.path.normpath(locale_path)
     translation = gettext.translation('Cura', locale_path, languages, fallback=True)
     translation.install(unicode=True)
