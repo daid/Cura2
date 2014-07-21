41e381076af86ed4178e488ba6c30384cef1412e
daid <daid303@gmail.com>
2014-07-21 15:29:15 +0200

    Move the save button to a different file.

---
+++ Cura/gui/widgets/printSaveButton.py
@@ -0,0 +1,67 @@
+import wx
+import os
+import threading
+
+from Cura.removableStorage import getStorageDevices
+from Cura.removableStorage import ejectDrive
+from Cura.gui.widgets.gradientButton import GradientButton
+
+
+class PrintSaveButton(GradientButton):
+    # TODO: This does not belong here. Move to seperate file
+    def __init__(self, parent, app):
+        self._app = app
+        super(PrintSaveButton, self).__init__(parent, label='Save on', icon='save_button.png', icon_align=wx.ALIGN_RIGHT)
+        self.Bind(wx.EVT_BUTTON, self._onSaveClick)
+        self._updateButton()
+        app.getTranslator().addProgressCallback(self._onProgressUpdate)
+
+    def _onProgressUpdate(self, progress, ready):
+        self.setFillAmount(progress)
+        self.Enable(ready)
+        self._updateButton()
+
+    def _updateButton(self):
+        if len(getStorageDevices()) > 0:
+            self.SetLabel('Save on')
+            self.setIcon('save_sd_button.png')
+        else:
+            self.SetLabel('Save')
+            self.setIcon('save_button.png')
+
+    def _onSaveClick(self, e):
+        storage_devices = getStorageDevices()
+        if len(storage_devices) > 0:
+            if len(storage_devices) == 1:
+                path = storage_devices[0][1]
+            else:
+                dlg = wx.SingleChoiceDialog(self, _("Select SD drive"), _("Multiple removable drives have been found,\nplease select your SD card drive"), map(lambda n: n[0], storage_devices))
+                if dlg.ShowModal() != wx.ID_OK:
+                    dlg.Destroy()
+                    return
+                path = storage_devices[dlg.GetSelection()][1]
+                dlg.Destroy()
+            filename = os.path.join(path, self._app.getScene().getResult().getDefaultFilename())
+
+            with open(filename, "wb") as f:
+                f.write(self._app.getScene().getResult().getGCode())
+            self._app.showNotification('Saved', 'Saved as %s' % (filename), lambda : threading.Thread(target=self._eject, args=(path,)).start())
+        else:
+            dlg = wx.FileDialog(self, _("Save toolpath"), style=wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT)
+            dlg.SetFilename(self._app.getScene().getResult().getDefaultFilename())
+            dlg.SetWildcard('Toolpath (*.gcode)|*.gcode')
+            if dlg.ShowModal() != wx.ID_OK:
+                dlg.Destroy()
+                return
+            filename = dlg.GetPath()
+            dlg.Destroy()
+
+            with open(filename, "wb") as f:
+                f.write(self._app.getScene().getResult().getGCode())
+
+    def _eject(self, drive):
+        ejectDrive(drive)
+        self._app.showNotification('Ejected', 'You can now remove the card')
+
+    def __del__(self):
+        self._app.getTranslator().removeProgressCallback(self._onProgressUpdate)
